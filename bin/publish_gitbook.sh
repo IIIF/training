97c307515c1cf2bbd2c02ae26cb835056037c42d
Jack Reed <phillipjreed@gmail.com>
2018-05-29 07:15:12 -0600

    update publish script

--- bin/publish_gitbook.sh
+++ bin/publish_gitbook.sh
@@ -4,6 +4,12 @@ cd intro-to-iiif
 # install the plugins and build the static site
 gitbook install && gitbook build
 
+# build 5 day workshop
+cd ../iiif-5-day-workshop
+gitbook install && gitbook build
+
+cd ../
+
 # checkout to the gh-pages branch
 git checkout gh-pages
 
@@ -11,7 +17,8 @@ git checkout gh-pages
 git pull origin gh-pages --rebase
 
 # copy the static site files into the current directory.
-cp -R _book/* .
+cp -R intro-to-iiif/_book/* ./intro-to-iiif
+cp -R iiif-5-day-workshop/_book/* ./iiif-5-day-workshop
 
 # remove 'node_modules' and '_book' directory
 git clean -fx node_modules
@@ -21,7 +28,7 @@ git clean -fx _book
 git add .
 
 # commit
-git commit -a -m "Update docs"
+git commit -a -m "Update site"
 
 # push to the origin
 git push origin gh-pages
