02cac2d143f05e66344ec2bbfd32573a69c7adbd
Glen Robson <glen.robson@gmail.com>
2020-05-29 04:29:22 +0100

    Moving bucket

--- .github/bin/createIndex.sh
+++ .github/bin/createIndex.sh
@@ -8,7 +8,7 @@ echo "<html><head><title>IIIF Training</title>" > build/index.html
 echo '<link rel="stylesheet" href="css/style.css" >' >> build/index.html
 echo '</head><body>' >> build/index.html >> build/index.html
 
-echo '<div class="container-lg px-3 my-5 markdown-body">' >> build/index.html
+echo '<div class="container-lg px-3 my-5 markdown-body">' >> build/index.html 
 cat build/content.html >> build/index.html
 echo '<script src="https://cdnjs.cloudflare.com/ajax/libs/anchor-js/4.1.0/anchor.min.js" integrity="sha256-lZaRhKri35AyJSypXXs4o6OPFTbTmUoltBbDCbdzegg=" crossorigin="anonymous"></script>
     <script>anchors.add();</script>' >> build/index.html
