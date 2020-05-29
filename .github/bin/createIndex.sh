ee66949eeabd7636bd688d3119f00099616e1e1f
Glen Robson <glen.robson@gmail.com>
2020-05-29 04:03:13 +0100

    Fixing permissions

--- .github/bin/createIndex.sh
+++ .github/bin/createIndex.sh
@@ -5,7 +5,7 @@ cp -r css build/
 marked -i index.md -o build/content.html
 
 echo "<html><head><title>IIIF Training</title>" > build/index.html
-echo '<link rel="stylesheet" href="" >' >> build/index.html
+echo '<link rel="stylesheet" href="css/style.css" >' >> build/index.html
 echo '</head><body>' >> build/index.html
 cat build/content.html >> build/index.html
 echo "</body></html>" >> build/index.html
