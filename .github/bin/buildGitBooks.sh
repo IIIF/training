fbaf9da638a3fa4db8177b2f715a52212820e1df
Glen Robson <glen.robson@gmail.com>
2020-05-30 16:33:01 +0100

    Catching errors and raising to ensure build fails

--- .github/bin/buildGitBooks.sh
+++ .github/bin/buildGitBooks.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
-mkdir build
+if [ ! -d "build" ]; then
+    mkdir build
+fi   
+# if this is 1 the build failed
+failed_build=0
+failure_message="Failed to build: "
 for dir in */ ; do
     cd $dir;
     count=`ls -1 *.md 2>/dev/null | wc -l`
     if [ $count != 0 ]; then
-        echo "$dir"
+        echo "Building $dir"
+        if [ -d "../build/$dir" ]; then
+            rm -rf "../build/$dir";
+        fi    
         mkdir "../build/$dir"
-        gitbook install && gitbook build . "../build/$dir"
+        gitbook install 
+        if [ $? -eq 1 ];then
+            failed_build=1
+            echo "Failed to install gitbook in $dir"
+            failure_message="$failure_message $dir"
+        fi
+        gitbook build . "../build/$dir"
+        if [ $? -eq 1 ];then
+            failed_build=1
+            echo "Failed to build gitbook in $dir"
+            failure_message="$failure_message $dir"
+        fi
     fi    
     cd ..
 done
 
-ls build
+if [ $failed_build -eq 1 ];then
+    echo "$failure_message"
+fi
+exit $failed_build
