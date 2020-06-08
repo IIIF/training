96a42d83d938e7000bf7eb172bbcaa5603d76fd8
Glen Robson <glen.robson@gmail.com>
2020-06-08 02:54:20 +0100

    Adding in progress Image API description

--- iiif-online-workshop/day-two/README.md
+++ iiif-online-workshop/day-two/README.md
@@ -1,111 +1,89 @@
 # Image API
 
-<script>
-    function showImage() {
-        identifier = getSelected('identifier');
-        region = getSelected('region');
-        size = getSelected('size');
-        rotation = getSelected('rotation');
-        quality = getSelected('quality');
-        format = getSelected('format');
-
-        url = identifier + "/" + region + "/" + size + "/" + rotation + "/" + quality + format;
-
-        link = document.getElementById('link');
-        link.href = url
-        link.innerHTML = url 
-
-        img = document.getElementById('iiif-image-api-img');
-        img.src = url
-    }
-
-    function getSelected(ident) {
-        select = document.getElementById(ident);
-        return select.options[select.selectedIndex].value;
-    }
+<script src="viewer.js" >
 </script>
 
+# Image API
 
-<ul class="demo">
-    <li>
-      <div class="attr" id="server">Identifier</div>
-      <div class="attr2">
-        <select id="identifier" onchange="showImage(); return true">
-            <option value="https://ids.lib.harvard.edu/ids/iiif/25286607">https://ids.lib.harvard.edu/ids/iiif/25286607</option>
-            <option value="https://dlcs.io/iiif-img/wellcome/5/b14658197.jp2">https://dlcs.io/iiif-img/wellcome/5/b14658197.jp2</option>
-            <option value="https://iiif.io/api/image/3.0/example/reference/9ee11092dfd2782634f5e8e2c87c16d5-uclamss_1841_diary_07_02">https://iiif.io/api/image/3.0/example/reference/9ee11092dfd2782634f5e8e2c87c16d5-uclamss_1841_diary_07_02</option>
-            <option value="https://ids.si.edu/ids/iiif/CHSDM-317E001E9E352-000001">https://ids.si.edu/ids/iiif/CHSDM-317E001E9E352-000001</option>
-            <option value="https://ids.si.edu/ids/iiif/SAAM-1930.12.50_1">https://ids.si.edu/ids/iiif/SAAM-1930.12.50_1</option>
-        </select>
-      </div>
-    </li>
-    <li>
-      <div class="attr">Region</div>
-      <div class="attr2">
-        <select id="region" class="changeIIIF" onchange="showImage(); return true">
-          <option value="full">full/</option>
-          <option value="square">square</option>
-          <option value="1000,100,3000,2000">1000,100,3000,2000</option>
-          <option value="2000,3000,2000,3000">2000,3000,2000,3000</option>
-        </select>
-      </div>
-    </li>
-    <li>
-      <div class="attr">Size</div><div class="attr2">
-        <select id="size" class="changeIIIF" onchange="showImage(); return true">
-          <option value="2000,">2000,/</option>
-          <option value="1000,">1000,/</option>
-          <option value="500," selected="selected">500,/</option>
-          <option value="250,">250,/</option>
-        </select>
-      </div>
-    </li>
-    <li>
-      <div class="attr">Rotation</div>
-      <div class="attr2">
-        <select id="rotation" class="changeIIIF" onchange="showImage(); return true">
-          <option value="0">0/</option>
-          <option value="45">45/</option>
-          <option value="90">90/</option>
-          <option value="135">135/</option>
-          <option value="180">180/</option>
-          <option value="225">225/</option>
-          <option value="270">270/</option>
-          <option value="315">315/</option>
-          <option value="!0">!0</option>
-          <option value="!180">!180</option>
-        </select>
-      </div>
-    </li>
-    <li>
-      <div class="attr">Quality</div>
-      <div class="attr2">
-        <select id="quality" class="changeIIIF" onchange="showImage(); return true">
-          <option value="default">default</option>
-          <option value="color">color</option>
-          <option value="gray">gray</option>
-          <option value="bitonal">bitonal</option>
-        </select>
-        </div>
-    </li>
-    <li>
-      <div class="attr">Format</div>
-      <div class="attr2">
-        <select id="format" class="changeIIIF" onchange="showImage(); return true">
-          <option value=".jpg">.jpg</option>
-          <option value=".png">.png</option>
-          <option value=".gif">.gif</option>
-        </select>
-      </div>
-    </li>
-</ul>
-<div style="margin-bottom:20px;">
-    <a id="link" class="centre" href="https://ids.lib.harvard.edu/ids/iiif/25286607/full/500,/0/default.jpg" id="image-url" target="_blank" rel="noopener noreferrer">https://ids.lib.harvard.edu/ids/iiif/25286607/full/500,/0/default.jpg</a>
-</div>
-<div class="iiif-image-api text-center">
-    <img class="centre" id="iiif-image-api-img" src="https://ids.lib.harvard.edu/ids/iiif/25286607/full/500,/0/native.jpg">
-</div>
+## Learning objectives
+ * Understand what the Image API provides
+ * Be able to change a Image API URL to get access to different parts of an image
+ * Understand the different types of Image API implementations (level0, level1 etc)
+ * Know the tools that work with the Image API so you can test your Image API server
 
+## Introduction
+
+ * What is it? A contract between the server and a viewing client
+ * Its the foundation of all of the IIIF APIs. They all build on top of the Image API.
+ * And luckily its one of the easiest to implement 
+ * because others have written servers you can install which provide the service
+
+## Terminology
+ * API - Application Programming Interface. In this case an agreed standard between the Image API Server and the Image API Client. 
+ * Client - The software which allows you to view a IIIF Image. As Web Browser don't support the Image API Natively there are many Javascript viewers that live on webpages that can allow you to view IIIF Images. Examples include OpenSeaDragon, Mirador and Universal Viewer. 
+ * Server - Software that runs on a machine at all times and is assessable over the Web. For this workshop I will mostly be referring to Image API Servers but there are other types of servers include Web servers or Email servers. Sometimes they are also know as Services e.g. Image API service, Web Service or Mail Service.  
+ * Image API - the agreed standard and specification which forms the contract between Client and Server
+ * Image API Server - the running software that provides access to images using the Image API. Sometimes shortened in this context to Image Servers. 
+
+## History
+
+IIIF really started out of the study of Medieval Manuscripts and the difficulty in using Digitised versions. Ben Albritton has written a great blog on the difficulties of working with images at that time and the promise IIIF gives in [Fellow Travelers: The Canterbury Tales and IIIF](https://blalbrit.github.io/2015/07/14/fellow-travelers-the-canterbury-tales-and-iiif). From a more personal perspective and also from a perspective of an institution providing these digital images. When I started work at the National Library of Wales, Manuscripts were digitised and three images were created per page. 
+
+1. A thumbnail:
+
+    ![Example thumbnail](https://damsssl.llgc.org.uk/iiif/2.0/image/4628571/full/90,/0/default.jpg)
+
+2. A Web or reference sized image:
+
+    ![Example web copy of image](https://damsssl.llgc.org.uk/iiif/2.0/image/4628571/full/300,/0/default.jpg)
+
+3. and a high quality archival Tiff that was only available on request. 
+
+These were made available through a viewer allowing you to browse around the Manuscript and switch between Thumbnail and Web view: 
+
+![Image of a historical digital viewer](imgs/Chaucer_viewer.png)
+
+As screens got bigger and the internet got faster the limited size of the Web images became a real barrier to use. Although the Archival Tiff was too big to put on the web, we needed some way to allow access to large images and support new methods of access like mobile browsers and the new uses scholars were making of the collection. The solution many institutions moved to was to create Zooming viewers like [Zoomify](http://www.zoomify.com/) and [Microsoft's DZI](https://en.wikipedia.org/wiki/Deep_Zoom). These viewers tended to use Flash which was unsupported on Mobile and required proprietary file formats to support. Around this time the JPEG2000 file format started to gain traction as an open file format that could support Zoomable viewers. 
+
+Enter:
+
+![IIIF Logo](imgs/logo.png)
+
+# Ideas
+ * go through image api parts - page per part with viewer
+ * level0
+ * info.json
+ * Users - homework, tile puzzle, National Museum of Sweden, Georeferencer
+ * New section look at ways you can use the image api
+   * Existing DAMS
+   * Hosted
+   * Install yourself 
+
+<div id="image_api_demo">
+</div>
 <script>
-    showImage()
+   addViewer({
+        div: 'image_api_demo',
+        images: [
+            'https://ids.lib.harvard.edu/ids/iiif/25286607'
+            ],
+        sizes: [
+            '500,',
+            '500,500',
+            '!500,500'
+        ],
+        regions: [
+            'full',
+            'square',
+            '1000,100,3000,2000',
+            '2000,3000,2000,2000',
+        ]
+   });
+   /*
+        'https://dlcs.io/iiif-img/wellcome/5/b14658197.jp2',
+            'https://iiif.io/api/image/3.0/example/reference/9ee11092dfd2782634f5e8e2c87c16d5-uclamss_1841_diary_07_02',
+            'https://ids.si.edu/ids/iiif/CHSDM-317E001E9E352-000001',
+            'https://ids.si.edu/ids/iiif/SAAM-1930.12.50_1'
+
+   */
 </script>    
---
+++ iiif-online-workshop/day-two/viewer.js
@@ -0,0 +1,521 @@
+infoJsons = {};
+
+/*
+ * Config:
+ * {
+ *      # identifer for div
+ *      div: "div_id", 
+ *      # IIIF Images to use
+ *      images: [
+ *          'http://example.com/image1'
+ *      ],
+ *      # Optional regions to use
+ *      regions: [
+            'full',
+            'square' 
+ *      ],
+        # Optional sizes
+        sizes: [
+            '500,'
+        ],
+        # Optional rotations
+        rotations: [
+            '45',
+            '90'
+        ],
+        # Optional qualities
+        qualities: [
+            'default',
+            'gray'
+        ]
+ * }
+ */
+function addViewer(config) {
+    addParams(config); 
+}
+
+function addParams(config) {
+    var div = document.getElementById(config.div);
+
+    var ul = createElementStyle('ul', 'demo');
+    div.appendChild(ul)
+    
+    addIdentifiers(ul, config);
+    addRegion(ul, config);
+    addSizes(ul, config);
+    addRotation(ul, config);
+    addQuality(ul, config);
+    addFormats(ul, config);
+
+    var linkDiv = document.createElement('div');
+    linkDiv.setAttribute('style', 'margin-bottom:20px');
+    div.appendChild(linkDiv);
+
+    var aLink = createElementStyle('a', 'centre');
+    aLink.id = "link";
+    aLink.setAttribute('target', '_blank');
+    aLink.setAttribute('rel', 'noopener noreferrer');
+    linkDiv.appendChild(aLink);
+
+    var imgDiv = createElementStyle('div', 'iiif-image-api text-center');
+    div.appendChild(imgDiv);
+
+    var img = createElementStyle('img', 'centre');
+    img.id = 'iiif-image-api-img';
+    imgDiv.appendChild(img);
+    showImage(config.div,null);
+}
+
+function cacheInfoJson(identifier) {
+    if (!(identifier in infoJsons)) {
+        var request = new XMLHttpRequest();
+        request.open('GET', identifier + '/info.json', false);  // `false` makes the request synchronous
+        request.send(null);
+
+        infoJsons[identifier] = JSON.parse(request.responseText);
+    }
+}
+
+function addIdentifiers(ul, config) {
+    var li = document.createElement('li');
+    ul.appendChild(li);
+    li.appendChild(createDescription('Identifier'));
+
+    var div = createElementStyle('div', 'attr2');
+    li.appendChild(div);
+
+    var select = createElementStyle('select','identifier');
+    select.id = 'identifier_' + config.div;
+    select.onchange = function(){  showImage(config.div,'identifier'); };
+    div.appendChild(select);
+
+    for (var i = 0; i < config.images.length; i++) {
+        cacheInfoJson(config.images[i]);
+    }
+    addOptions('identifier_' + config.div, config.images, '', '/');
+}
+
+function addRegion(ul, config) {
+    var li = document.createElement('li');
+    ul.appendChild(li);
+    li.appendChild(createDescription('Region'));
+
+    var div = createElementStyle('div', 'attr2');
+    li.appendChild(div);
+
+    var select = createElementStyle('select','region');
+    select.id = 'region_' + config.div;
+    select.onchange = function(){ showImage(config.div,'region'); };
+    div.appendChild(select);
+
+    var regions = null;
+    if ('regions' in config) {
+        // Use supplied regions
+        regions = config.regions;
+    }
+    updateRegion(config.div, regions);
+}
+
+function clearSelect(select) {
+   var i, L = select.options.length - 1;
+   for(i = L; i >= 0; i--) {
+      select.remove(i);
+   }
+}
+
+function updateRegion(uuid, suppliedRegions) {
+    if (suppliedRegions) {
+        var regions = suppliedRegions;
+    } else {
+        var regions = ['full'];
+        if (supports(getCurrentImage(uuid), 'regionSquare')) {
+            regions.push('square');
+        }
+        var infoJson = infoJsons[getCurrentImage(uuid)];
+        if (infoJson.width > 1500 && infoJson.height > 1500) {
+            centerX = Math.floor(infoJson.width / 2);
+            centerY = Math.floor(infoJson.height / 2);
+            regions.push((centerX - 500) + "," + (centerY - 500) + ",1000,1000");
+        }
+
+    }
+    addOptions('region_' + uuid, regions, '', '/');
+}
+function addSizes(ul, config) {
+    var li = document.createElement('li');
+    ul.appendChild(li);
+    li.appendChild(createDescription('Size'));
+
+    var div = createElementStyle('div', 'attr2');
+    li.appendChild(div);
+
+    var select = createElementStyle('select','size');
+    select.id = 'size_' + config.div;
+    select.onchange = function(){ showImage(config.div,'size'); };
+    div.appendChild(select);
+
+    if ('sizes' in config) {
+        // Use supplied sizes
+        var sizes = config.sizes;
+    } else {
+        var sizes = ['250,', '500,', '1000,'];
+        if (serviceVersion(getCurrentImage(config.div)) == '2') {
+            sizes.push('full');
+        } else {
+            sizes.push('max');
+        }
+    }
+
+    addOptions('size_' + config.div, sizes, '', '/');
+}
+
+function addRotation(ul, config) {
+    var li = document.createElement('li');
+    ul.appendChild(li);
+    li.appendChild(createDescription('Rotation'));
+
+    var div = createElementStyle('div', 'attr2');
+    li.appendChild(div);
+
+    var select = createElementStyle('select','rotation');
+    select.id = 'rotation_' + config.div;
+    select.onchange = function(){ showImage(config.div,'rotation'); };
+    div.appendChild(select);
+
+    var rotations = null;
+    if ('rotations' in config) {
+        // Use supplied rotations
+        rotations = config.rotations;
+    }
+    updateRotation(config.div, rotations);
+}
+
+function updateRotation(uuid, suppliedRotations) {
+    if (suppliedRotations) {
+        var rotations = suppliedRotations;
+    } else {
+        var rotations = ['0'];
+        if (supports(getCurrentImage(uuid), 'rotationBy90s')) {
+            rotations.push('90');
+            rotations.push('180');
+            rotations.push('270');
+        }
+        if (supports(getCurrentImage(uuid), 'rotationArbitrary')) {
+            rotations.push('45');
+            rotations.push('135');
+        }
+        if (supports(getCurrentImage(uuid), 'mirroring')) {
+            rotations.push('!0');
+            rotations.push('!180');
+        }
+    }
+    addOptions('rotation_' + uuid, rotations, '', '/');
+}
+
+function addOptions(identifier, options, prepend, append) {
+    var select = document.getElementById(identifier);
+    clearSelect(select);
+    for (var i = 0; i < options.length; i++) {
+        var optionValue = options[i];
+        option = document.createElement('option');
+        option.text = prepend + optionValue + append;
+        option.value = optionValue;
+
+        select.appendChild(option);
+    }
+}
+
+function addQuality(ul, config) {
+    var li = document.createElement('li');
+    ul.appendChild(li);
+    li.appendChild(createDescription('Quality'));
+
+    var div = createElementStyle('div', 'attr2');
+    li.appendChild(div);
+
+    var select = createElementStyle('select','quality');
+    select.id = 'quality_' + config.div;
+    select.onchange = function(){ showImage(config.div,'quality'); };
+    div.appendChild(select);
+
+    var qualities = null;
+    if ('qualities' in config) {
+        // Use supplied qualities
+        qualities = config.qualities;
+    }
+    updateQuality(config.div, qualities);
+}
+
+function updateQuality(uuid, suppliedQualities) {
+    var select = document.getElementById('quality_' + uuid);
+    clearSelect(select);
+    if (suppliedQualities) {
+        var qualities = suppliedQualities;
+    } else {
+        var qualities = getImageQualities(uuid);
+    }
+    addOptions('quality_' + uuid, qualities, '', '');
+}
+
+function addFormats(ul, config) {
+    var li = document.createElement('li');
+    ul.appendChild(li);
+    li.appendChild(createDescription('Format'));
+
+    var div = createElementStyle('div', 'attr2');
+    li.appendChild(div);
+
+    var select = createElementStyle('select','format');
+    select.id = 'format_' + config.div;
+    select.onchange = function(){ showImage(config.div,'format'); };
+    div.appendChild(select);
+
+    var formats = null;
+    if ('formats' in config) {
+        // Use supplied formats
+        formats = config.formats;
+    }
+    updateFormats(config.div, formats);
+}
+
+function updateFormats(uuid, suppliedFormats) {
+    var select = document.getElementById('format_' + uuid);
+    clearSelect(select);
+    if (suppliedFormats) {
+        var formats = suppliedFormats;
+    } else {
+        var formats = getImageFormats(uuid);
+    }
+    addOptions('format_' + uuid, formats, '.', '');
+}
+
+function serviceVersion(image) {
+    var infoJson = infoJsons[image];
+    var version = '2';
+    if ('type' in infoJson && infoJson['type'] == "ImageService3") {
+        version = '3';
+    }
+    return version;
+}
+
+function getImageFormats(uuid) {
+    var image = getCurrentImage(uuid);
+    var infoJson = infoJsons[image];
+
+    var version = serviceVersion(image);
+    var formats = ['jpg'];
+    if (version == '2') {
+        if ('profile' in infoJson) {
+            for (i = 0; i < infoJson.profile.length; i++) {
+                if (typeof infoJson.profile[i] === 'object') {
+                    if ('formats' in infoJson.profile[i]) {
+                        for (var j = 0; j < infoJson.profile[i]['formats'].length; j++) {
+                            if (!formats.includes(infoJson.profile[i]['formats'][j])) {
+                                formats.push(infoJson.profile[i]['formats'][j]);
+                            }
+                        }
+                    }    
+                } else {
+                    if (infoJson.profile[i] == "http://iiif.io/api/image/2/level2.json" && !(formats.includes('png'))) {
+                        formats.push('png');
+                    }
+                }
+            }
+        }
+    } else {
+        if (infoJson['profile'] == 'level2') {
+            formats.push('png');
+        }
+        if ('extraFormats' in infoJson) {
+            for (var i = 0; i < infoJson['extraFormats']; i++) {
+                if (!formats.includes(infoJson['extraFormats'][i])) {
+                    formats.push(infoJson['extraFormats'][i]);
+                }
+            }
+        }    
+    }
+    return formats;
+}
+
+function getImageQualities(uuid) {
+    var image = getCurrentImage(uuid);
+    var infoJson = infoJsons[image];
+
+    var version = serviceVersion(image);
+    if (version == '2') {
+        if ('profile' in infoJson) {
+            for (i = 0; i < infoJson.profile.length; i++) {
+                if (typeof infoJson.profile[i] === 'object' && 'qualities' in infoJson.profile[i]) {
+                    return infoJson.profile[i]['qualities'];
+                }
+            }
+        }
+        return ['default'];
+    } else {
+        if ('extraQualities' in infoJson) {
+            return infoJson['extraQualities']
+        } else {
+            return ['default'];
+        }    
+    }
+}
+
+function supports(image, feature) {
+    var infoJson = infoJsons[image];
+
+    var version = serviceVersion(image);
+    if (version == '2') {
+        if ('profile' in infoJson) {
+            for (i = 0; i < infoJson.profile.length; i++) {
+                if (typeof infoJson.profile[i] === 'object' && 'supports' in infoJson.profile[i]) {
+                    return infoJson.profile[i]['supports'].includes(feature);
+                }
+            }
+        }
+        return false;
+    } else {
+        if ('extraFeatures' in infoJson) {
+            return infoJson['extraFeatures'].includes(feature);
+        }
+        return false;
+    }
+}
+
+function getCurrentImage(ident) {
+    var select = document.getElementById('identifier_' + ident);
+    return select.options[select.selectedIndex].value;
+}
+
+function createDescription(text) {
+    var div = document.createElement('div');
+    div.setAttribute('class', 'attr');
+    div.innerHTML = text;
+
+    return div;
+}
+
+function createElementStyle(element, style) {
+    var el = document.createElement(element);
+    el.setAttribute('class', style);
+
+    return el;
+}
+    
+ /*   <li>
+      <div class="attr">Region</div>
+      <div class="attr2">
+        <select id="region" class="changeIIIF" onchange="showImage(); return true">
+          <option value="full">full/</option>
+          <option value="square">square</option>
+          <option value="1000,100,3000,2000">1000,100,3000,2000</option>
+          <option value="2000,3000,2000,3000">2000,3000,2000,3000</option>
+        </select>
+      </div>
+    </li>
+    <li>
+      <div class="attr">Size</div><div class="attr2">
+        <select id="size" class="changeIIIF" onchange="showImage(); return true">
+          <option value="2000,">2000,/</option>
+          <option value="1000,">1000,/</option>
+          <option value="500," selected="selected">500,/</option>
+          <option value="250,">250,/</option>
+        </select>
+      </div>
+    </li>
+    <li>
+      <div class="attr">Rotation</div>
+      <div class="attr2">
+        <select id="rotation" class="changeIIIF" onchange="showImage(); return true">
+          <option value="0">0/</option>
+          <option value="45">45/</option>
+          <option value="90">90/</option>
+          <option value="135">135/</option>
+          <option value="180">180/</option>
+          <option value="225">225/</option>
+          <option value="270">270/</option>
+          <option value="315">315/</option>
+          <option value="!0">!0</option>
+          <option value="!180">!180</option>
+        </select>
+      </div>
+    </li>
+    <li>
+      <div class="attr">Quality</div>
+      <div class="attr2">
+        <select id="quality" class="changeIIIF" onchange="showImage(); return true">
+          <option value="default">default</option>
+          <option value="color">color</option>
+          <option value="gray">gray</option>
+          <option value="bitonal">bitonal</option>
+        </select>
+        </div>
+    </li>
+    <li>
+      <div class="attr">Format</div>
+      <div class="attr2">
+        <select id="format" class="changeIIIF" onchange="showImage(); return true">
+          <option value=".jpg">.jpg</option>
+          <option value=".png">.png</option>
+          <option value=".gif">.gif</option>
+        </select>
+      </div>
+    </li>
+</ul>
+<div style="margin-bottom:20px;">
+    <a id="link" class="centre" href="https://ids.lib.harvard.edu/ids/iiif/25286607/full/500,/0/default.jpg" id="image-url" target="_blank" rel="noopener noreferrer">https://ids.lib.harvard.edu/ids/iiif/25286607/full/500,/0/default.jpg</a>
+</div>
+<div id="iiif-image-api-div" class="iiif-image-api text-center">
+    <a id="highlight">
+    </a>
+</div>
+<div class="iiif-image-api text-center">
+    <img class="centre" id="iiif-image-api-img" src="https://ids.lib.harvard.edu/ids/iiif/25286607/full/500,/0/native.jpg">
+</div>*/
+
+
+
+function showImage(uuid, source) {
+    if (source == 'identifier') {
+        updateRegion(uuid, null);
+        updateRotation(uuid, null);
+        updateQuality(uuid, null);
+        updateFormats(uuid, null);
+    }
+    var identifier = getSelected(uuid, 'identifier');
+    var region = getSelected(uuid, 'region');
+    var size = getSelected(uuid, 'size');
+    var rotation = getSelected(uuid, 'rotation');
+    var quality = getSelected(uuid, 'quality');
+    var format = getSelected(uuid, 'format');
+
+    var url = identifier + "/" + region + "/" + size + "/" + rotation + "/" + quality + "." + format;
+
+    var link = document.getElementById('link');
+    link.href = url
+    link.innerHTML = url 
+
+    //var img = document.getElementById('iiif-image-api-div');
+    //img.style.backgroundImage = "url(" + identifier + "/full/" + size + "/0/" + quality + format + ")";
+
+    /*var highlight = document.getElementById('highlight');
+    if (region.indexOf(',') != -1) {
+        coords = region.split(',');
+        highlight.style.top = coords[0] + "px"; 
+        highlight.style.left = coords[1] + "px"; 
+        highlight.style.width = coords[2] + "px"; 
+        highlight.style.height = coords[3] + "px"; 
+    } else if (region ==  "full") {
+        highlight.style.top = "0px"; 
+        highlight.style.left = "0px"; 
+        highlight.style.width = img.offsetWidth; 
+        highlight.style.height = img.offsetHeight; 
+    }*/
+
+    var img = document.getElementById('iiif-image-api-img');
+    img.src = url;
+}
+
+function getSelected(uuid, ident) {
+    var select = document.getElementById(ident + '_' + uuid);
+    return select.options[select.selectedIndex].value;
+}
