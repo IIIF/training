8293218a0e9e921ae50577fde5780a46961a076b
Glen Robson <glen.robson@gmail.com>
2022-02-18 01:54:31 +0000

    Updating file last mod date from github

---
+++ .github/bin/updateLastMod.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+resolveLink() {
+    local filename="$1"
+    cwd=`pwd`
+    cd `dirname $filename`
+    fullpath=`pwd -P`/`basename $filename`
+
+    cd $cwd
+    retval="$fullpath"
+}
+
+updateFile() {
+    local filename="$1"
+    local sourceFilename=""
+    if [[ "$filename" == *"index.html"* ]]; then
+      sourceFilename="${filename/index.html/README.html}"
+    else
+      sourceFilename="$filename"
+    fi
+    noBuild="${sourceFilename/build\//}"
+    # If html version exists use that one
+    if test -f "$noBuild"; then
+        mdFile="$noBuild"
+    else
+        mdFile="${noBuild/.html/.md}"
+    fi
+    resolveLink $mdFile
+    mdFile="$retval"
+
+    modification=`git log -1 --pretty="format:%cd" --date local $mdFile `
+    echo "Filename $filename lastmod: $modification sourceFile: $mdFile"
+    if [ -n "$modification" ];then
+        touchFormat=`date -jf "%a %b %e %T %Y" "$modification" +"%FT%T"`
+        echo "touch -d \"$touchFormat\" $filename "
+        touch -d "$touchFormat" $filename
+    else
+        echo "NO_DATE"
+    fi    
+}
+
+export -f updateFile
+export -f resolveLink
+find build -name "*.html" -exec bash -c "updateFile {}" \;
